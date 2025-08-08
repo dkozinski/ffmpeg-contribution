808a4444036bc8b0ba9f4bf4ee67b9265531fc0f
Dawid Kozinski <d.kozinski@samsung.com>
2025-08-08 10:47:35 +0200

    Updated build script for APV shared debug build
    Signed-off-by: Dawid Kozinski <d.kozinski@samsung.com>

---
+++ build/apv/build_shared_debug_fate.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --samples=fate-suite/ --extra-libs="-lpthread -lm" --bindir="$HOME/ffmpeg_build/bin" --enable-ffplay --enable-shared --disable-static --enable-liboapv --enable-gpl --enable-libx264 --enable-libx265 --disable-optimizations --disable-mmx --disable-stripping --extra-cflags=-Og --extra-cflags=-fno-omit-frame-pointer --enable-debug=3 --extra-cflags=-fno-inline && make -j $(nproc) && make install && make fate-rsync && LD_LIBRARY_PATH="$HOME/ffmpeg_build/lib/" make fate -j 8
+
+#PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --samples=fate-suite/ --extra-libs="-lpthread -lm" --bindir="$HOME/ffmpeg_build/bin" --enable-ffplay --enable-shared --disable-static --enable-liboapv --enable-gpl --enable-libx264 --enable-libx265 --disable-optimizations --disable-mmx --disable-stripping --extra-cflags=-Og --extra-cflags=-fno-omit-frame-pointer --enable-debug=3 --extra-cflags=-fno-inline && make -j $(nproc) && make install
+
+
+#PKG_CONFIG_PATH=${HOME}/ffmpeg_build/lib/pkgconfig/ ./configure  --prefix="$HOME/ffmpeg_build" --enable-shared --enable-libapv --enable-ffplay && make -j8 && make install
+
+#PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --extra-libs="-lpthread -lm" --bindir="$HOME/bin" --enable-ffplay --enable-gpl --enable-gnutls --enable-libfreetype --enable-libapv --enable-libx264 --enable-nonfree --disable-optimizations --disable-mmx --disable-stripping --extra-cflags=-Og --extra-cflags=-fno-omit-frame-pointer --enable-debug=3 --extra-cflags=-fno-inline && PATH="$HOME/bin:$PATH" make -j $(nproc) && make install
