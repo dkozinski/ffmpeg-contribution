c69f2726caef04a9b050ecc3605b7e49ca6e2b52
Dawid Kozinski <d.kozinski@samsung.com>
2025-10-21 14:26:46 +0200

    Refactoring debug and release configurations for Linux and the release configuration for Windows
    Signed-off-by: Dawid Kozinski <d.kozinski@samsung.com>

--- build/apv/ffapv/build_shared_release.sh
+++ build/apv/ffapv/build_shared_release.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffapv_build/lib/pkgconfig" ./configure --prefix="$HOME/ffapv_build" --extra-libs="-lpthread -lm" --enable-ffplay --enable-shared --disable-static --enable-liboapv --enable-gpl --enable-libx264 && make -j $(nproc) && make install
-
-#PKG_CONFIG_PATH=${HOME}/ffmpeg_build/lib/pkgconfig/ ./configure  --prefix="$HOME/ffapv_build" --enable-shared --enable-libapv --enable-ffplay && make -j8 && make install
-
-#PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --pkg-config-flags="--static" --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --extra-libs="-lpthread -lm" --bindir="$HOME/bin" --enable-ffplay --enable-gpl --enable-gnutls --enable-libfreetype --enable-libapv --enable-libx264 --enable-nonfree --disable-optimizations --disable-mmx --disable-stripping --extra-cflags=-Og --extra-cflags=-fno-omit-frame-pointer --enable-debug=3 --extra-cflags=-fno-inline && PATH="$HOME/bin:$PATH" make -j $(nproc) && make install
+# This script builds FFmpeg with shared libraries and debug symbols.(with fate tests, debug symbols, and optimizations disabled)
+PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/build_ubuntu/lib/pkgconfig" ./configure \
+ --prefix="$HOME/build_ubuntu" \
+ --samples=fate-suite/ \
+ --extra-libs="-lpthread -lm" \
+ --bindir="$HOME/build_ubuntu/bin" \
+ --enable-ffplay \
+ --enable-shared \
+ --disable-static \
+ --enable-liboapv \
+ --enable-gpl \
+ --enable-libx264 \
+ --enable-libx265 \
+ --enable-mmx \
+ --enable-stripping \
+ --extra-cflags="-O3 -fno-inline -fno-omit-frame-pointer" \
+ --optflags="-O3" \
+ --disable-debug \
+ && make -j $(nproc) && make install
--- build/apv/ffapv/build_shared_release_windows.sh
+++ build/apv/ffapv/build_shared_release_windows.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-PKG_CONFIG_PATH=${HOME}/windows_build/lib/pkgconfig/ ./configure --disable-static --enable-shared --prefix="$HOME/ffapv_build_windows" --arch=x86_64 --target-os=mingw32 --cross-prefix=x86_64-w64-mingw32- --enable-sdl2 --enable-liboapv --enable-gpl --enable-libx264 --enable-ffplay && make -j8 && make install
+PKG_CONFIG_PATH=${HOME}/windows_build/lib/pkgconfig/ ./configure \
+	--disable-static \
+	--enable-shared \
+	--prefix="$HOME/ffapv_build_windows" \
+	--arch=x86_64 \
+	--target-os=mingw32 \
+	--cross-prefix=x86_64-w64-mingw32- \
+	--enable-sdl2 \
+	--enable-liboapv \
+	--enable-gpl \
+	--enable-libx264 \
+	--enable-ffplay && make -j8 && make install
 
 
 #PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffapv_build_static" --pkg-config-flags="--static" --extra-libs="-lpthread -lm" --enable-static --disable-shared --enable-ffplay --enable-libapv && make -j $(nproc) && make install
